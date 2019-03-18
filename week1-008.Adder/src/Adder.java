97cfb3fb1c40bc67da242e8f9f0c7bce3021ab2e
Igor Kajtez <39498011+igorkajtez@users.noreply.github.com>
2019-03-18 19:50:38 +0100

    Complete Adder

---
+++ week1-008.Adder/src/Adder.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Adder {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        
+        System.out.print("Type a number: ");
+        int a = reader.nextInt();
+        System.out.print("Type another number: ");
+        int b = reader.nextInt();
+        
+        int sum = a + b;
+        System.out.println("Sum of the numbers: " + sum);        
+       
+    }
+}
