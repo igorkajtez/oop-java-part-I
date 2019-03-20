bc8499bfe59e0058c846167d1c27d79aa97f562d
Igor Kajtez <39498011+igorkajtez@users.noreply.github.com>
2019-03-20 14:54:04 +0100

    Complete

---
+++ week2/week2-036.LoopsEndingRemembering/src/LoopsEndingRemembering.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class LoopsEndingRemembering {
+    public static void main(String[] args) {
+        // program in this project exercises 36.1-36.5
+        // actually this is just one program that is split in many parts
+        
+        Scanner reader = new Scanner(System.in);
+        
+        int number = 0;
+        int sum = 0;
+        int counter = 0;
+        int even = 0;
+        int odd = 0;
+        
+        System.out.println("Type numbers:");
+        while(number!=-1){
+            number = reader.nextInt();
+            if(number!=-1){               
+                sum += number;
+                counter++;
+                if(number%2==0){
+                    even++;
+                }else {
+                    odd++;
+                }
+            }            
+        }
+        
+        System.out.println("Thank you and see you later!");
+        System.out.println("The sum is " + sum);
+        System.out.println("How many numbers: " + counter);
+        System.out.println("Average is: " + 1.0 * sum / counter);
+        System.out.println("Even numbers: " + even);
+        System.out.println("Odd numbers: " + odd);
+
+    }
+}
