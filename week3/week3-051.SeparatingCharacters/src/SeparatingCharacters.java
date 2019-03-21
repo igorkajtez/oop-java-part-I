8e8c530e26f894a35b30f97f2ebd3b3888415640
Igor Kajtez <39498011+igorkajtez@users.noreply.github.com>
2019-03-21 19:41:25 +0100

    Complete

---
+++ week3/week3-051.SeparatingCharacters/src/SeparatingCharacters.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class SeparatingCharacters {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        
+        System.out.println("Type your name:");
+        String name = reader.nextLine();
+        
+        int i = 0;
+        while(i < name.length()){
+            char nameChar = name.charAt(i);
+            System.out.println(i+1 + ". character: " + nameChar);
+            i++;
+            
+        }
+    }
+}
