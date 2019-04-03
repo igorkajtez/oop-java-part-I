5c63c746199bd7db54fb5cd0fb964426e1a5c33c
igorkajtez <kajtezigor@gmail.com>
2019-04-03 14:05:19 +0200

    Complete ClockObject

---
+++ week5/week5-089.ClockObject/src/BoundedCounter.java
@@ -0,0 +1,44 @@
+
+public class BoundedCounter {
+    private int value;
+    private int upperLimit;
+
+    public BoundedCounter(int upperLimit) {
+        this.value = 0;
+        this.upperLimit = upperLimit;
+    }
+    
+    public int getValue(){
+        return this.value;
+    }
+    
+    public void setValue(int value){
+        if(value < 0 || value > upperLimit){
+            return;
+        }
+        this.value = value;
+    }
+    
+    public void next(){
+        if(this.value == this.upperLimit){
+            this.value = 0;
+            return;
+        }
+        this.value++;
+        
+    }
+
+    @Override
+    public String toString() {
+        if(this.value < 10){
+            return "0" + this.value;
+        }
+        return "" + this.value;
+        
+    }
+    
+    
+    
+    
+
+}
