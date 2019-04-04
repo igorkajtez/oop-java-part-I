8df29a6f962261081b4b2453007aa7d94e30e01b
igorkajtez <kajtezigor@gmail.com>
2019-04-04 22:41:28 +0200

    Complete ElegantPrintitng

---
+++ week6/week6-097.ElegantPrinting/src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = {5, 1, 3, 4, 2};
+        printElegantly(array);
+    }
+
+//    public static void printElegantly(int[] array) {
+//        for (int i = 0; i < array.length; i++) {
+//            System.out.print(array[i]);
+//            if (i < (array.length - 1)) {
+//                System.out.print(", ");
+//            }
+//        }        
+//    }
+    
+    public static void printElegantly(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            String s = i < array.length-1 ? ", " : "";
+            System.out.print(array[i] + s);
+        }        
+    }
+}
